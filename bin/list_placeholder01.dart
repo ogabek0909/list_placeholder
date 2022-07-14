d75e2a467ec4740d4948cc21b117b3ba956c19ed
ogabek <norpulatovogabek42@gmail.com>
2022-07-14 21:31:35 +0500

    sdf

--- bin/list_placeholder01.dart
+++ bin/list_placeholder01.dart
@@ -4,5 +4,12 @@ Create a list with the following values: 1, 2, 3, 4, 5
 Return the List
 
 */
+List func() {
+  List a = [];
+  a.addAll([1, 2, 3, 4, 5]);
+  return a;
+}
 
-void main() {}
+void main() {
+  print(func());
+}
