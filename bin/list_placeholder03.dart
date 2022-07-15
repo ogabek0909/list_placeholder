9876d53740a2fcc8b96ce7ac64b7ede31ad954fa
ogabek <norpulatovogabek42@gmail.com>
2022-07-15 19:39:07 +0500

    dfsds

---
+++ bin/list_placeholder03.dart
@@ -0,0 +1,15 @@
+/*
+Create function func 
+Make a list of n ones.
+Argument int n
+Return the List
+
+*/
+List func(int n) {
+  List a = List.filled(n, 0);
+  return a;
+}
+
+void main() {
+  print(func(4));
+}
