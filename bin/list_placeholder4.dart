117ee3875818a2714f29b506f64a617ba5fff3d7
ogabek <norpulatovogabek42@gmail.com>
2022-07-15 11:40:43 +0500

    redone

--- bin/list_placeholder4.dart
+++ bin/list_placeholder4.dart
@@ -5,5 +5,16 @@ Argument int n
 Return the List
 
 */
+List func(int n) {
+  List a = [];
+  int q = 0;
+  while (q < n) {
+    q++;
+    a.add(q);
+  }
+  return a;
+}
 
-void main() {}
+void main() {
+  print(func(4));
+}
