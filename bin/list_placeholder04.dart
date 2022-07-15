d9f6095e9782c032af37445eae6b4d423abc0700
ogabek <norpulatovogabek42@gmail.com>
2022-07-15 19:39:23 +0500

    dfsds

---
+++ bin/list_placeholder04.dart
@@ -0,0 +1,20 @@
+/*
+Create function func 
+Create a list ranging from 0 to n.
+Argument int n
+Return the List
+
+*/
+List func(int n) {
+  List a = [];
+  int q = 0;
+  while (q < n) {
+    q++;
+    a.add(q);
+  }
+  return a;
+}
+
+void main() {
+  print(func(5));
+}
