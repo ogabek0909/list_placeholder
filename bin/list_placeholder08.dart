6b2fcfd82cefb949f4d45f568296466068bcc0b1
ogabek <norpulatovogabek42@gmail.com>
2022-07-16 09:14:51 +0500

    task8

--- bin/list_placeholder08.dart
+++ bin/list_placeholder08.dart
@@ -5,5 +5,16 @@ Argument int n
 Return the List
 
 */
+List func(int n) {
+  List y = [];
+  int i = 0;
+  while (i < n) {
+    y.add(i);
+    i += 2;
+  }
+  return y;
+}
 
-void main() {}
+void main() {
+  print(func(5));
+}
